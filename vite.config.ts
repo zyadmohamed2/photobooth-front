c812eb3cb32ce4259a5e59d5581326ebbf27556b
zyadmohamedlskdfj <zyadmohamd456@gmail.com>
2025-01-22 06:29:25 +0200

    lkdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // هذا يسمح بالوصول من أي عنوان IP
+    port: 8080
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
